fca451154cf7e1f553d0a36d5559cf3ca630e4c3
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-03 22:12:01 +0300

    first Project

---
+++ First Project/Duration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionOOP04.First_Project
+{
+    internal class Duration
+    {
+        public int Hours;
+        public int Minutes;
+        public int Seconds;
+
+       public Duration(int hours,int minutes,int seconds)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+
+        public  Duration(int totalseconds)
+        {
+            Hours = totalseconds / 3600;
+            Minutes = (totalseconds % 3600)%60;
+            Seconds = totalseconds % 60;
+        }
+
+        public void Durationfromseconds(int hours,int minutes,int seconds)
+        {
+            Hours = hours + minutes / 60 + seconds / 3600;
+            Minutes = (minutes + (seconds / 60)) % 60;
+            Seconds = seconds % 60;
+        }
+        public int ToSeconds()
+        {
+            return (Hours * 3600) +( Minutes * 60) + Seconds;
+        }
+
+        public override string ToString()
+        {
+            return $"Hours : {Hours} :: Minutes : {Minutes} :: Seconds : {Seconds}";
+        }
+
+        public static Duration operator +(Duration d1,Duration d2)
+        {
+            int totalhours = d1.Hours + d2.Hours;
+            int totalminutes = d1.Minutes + d2.Minutes;
+            int totalseconds = d1.Seconds + d2.Seconds;
+            return new Duration(totalhours, totalminutes, totalseconds);
+        }
+
+        public static Duration operator -(Duration d1, Duration d2)
+        {
+            int totalhours = d1.Hours - d2.Hours;
+            int totalminutes = d1.Minutes - d2.Minutes;
+            int totalseconds = d1.Seconds - d2.Seconds;
+            return new Duration(totalhours, totalminutes, totalseconds);
+        }
+
+        public static Duration operator ++(Duration d)
+        {
+            return new Duration(d.Hours, d.Minutes+1, d.Seconds);
+        }
+
+        public static Duration operator --(Duration d)
+        {
+            return new Duration(d.Hours, d.Minutes-1, d.Seconds);
+        }
+
+        public static bool operator > (Duration d1,Duration d2)
+        {
+            return d1.ToSeconds() > d2.ToSeconds();
+        }
+
+        public static bool operator <(Duration d1, Duration d2)
+        {
+            return d1.ToSeconds() < d2.ToSeconds();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SessionOOP04
+﻿using SessionOOP04.First_Project;
+
+namespace SessionOOP04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //First Project 
+
+            Duration D1 = new Duration(1, 10, 15);
+            Console.WriteLine(D1.ToString());
+
+
+            Duration D2 = new Duration(3600);
+            Console.WriteLine(D2.ToString());
+
+
+            Duration D3 = new Duration(7800);
+            Console.WriteLine(D3.ToString());
+
+
+            Duration D4 = new Duration(3600);
+            Console.WriteLine(D4.ToString());
+
+            D3 = D1 + D2;
+            Console.WriteLine(D3.ToString());
+
+            D3 = D1 +D4;
+            Console.WriteLine(D3.ToString());
+
+            D3++;
+            Console.WriteLine(D3.ToString());
+
+            D3--;
+            Console.WriteLine(D3.ToString());
+
+            Console.WriteLine($"{D1 > D2}");
+
         }
     }
 }
